ed8895abf37b9f358399b8f660634355bbbb7619
Caner Polat <cpcanerpolat@gmail.com>
2018-10-26 23:53:16 +0300

    satır düzenlemesi yapıldı.

--- src/com/caner/ws/GaleriOperation.java
+++ src/com/caner/ws/GaleriOperation.java
@@ -17,37 +17,33 @@ public class GaleriOperation {
 
 		ConnectionDatabase connectionDatabase = new ConnectionDatabase();
 		getConnect = connectionDatabase.getConnection("Ogrenci_db", "root", "");
-
 		try {
 			System.out.println("insert block");
 			String sqlInsert = "INSERT INTO galeri(carName,carModel,carColour) VALUES(?,?,?)";
-
 			PreparedStatement statement = getConnect.prepareStatement(sqlInsert);
 			statement.setString(1, galeriEntity.getCarName());
 			statement.setInt(2, galeriEntity.getCarModel());
 			statement.setString(3, galeriEntity.getCarColour());
 			int rowsInserted = statement.executeUpdate();
 			if (rowsInserted > 0)
-				System.out.println("---insert eklendi---");
+				System.out.println("insert operation succesful");
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			getConnect.close();
 		}
+
 	}
 
 	public JsonArray selectDatabase() throws Throwable {
 
 		ConnectionDatabase connectionDatabase = new ConnectionDatabase();
 		getConnect = connectionDatabase.getConnection("Ogrenci_db", "root", "");
-
 		try {
-			System.out.println("Select Block");
 			String sqlSelect = "SELECT * FROM galeri";
 			preparedStatement = getConnect.prepareStatement(sqlSelect);
 			ResultSet resultSet = preparedStatement.executeQuery();
 			JsonArray jsonArray = new JsonArray();
-
 			while (resultSet.next()) {
 
 				JsonObject jsonObject = new JsonObject();
@@ -57,8 +53,10 @@ public class GaleriOperation {
 				jsonObject.addProperty("carModel", resultSet.getInt("carModel"));
 				jsonArray.add(jsonObject);
 			}
+
 			System.out.println("Array type : " + jsonArray);
 			return jsonArray;
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
@@ -66,28 +64,26 @@ public class GaleriOperation {
 			getConnect.close();
 		}
 	}
+
 	public void updateDatabase(GaleriEntity galeriEntity) throws SQLException {
-		
+
 		ConnectionDatabase connectionDatabase = new ConnectionDatabase();
 		getConnect = connectionDatabase.getConnection("Ogrenci_db", "root", "");
+
 		try {
-			System.out.println("---update block---");
-			String sqlUpdate = "UPDATE galeri "
-					+ "SET carName = ?, carModel = ?, carColour = ? "
-					+ "WHERE carId = ?";
+			System.out.println("update block");
+			String sqlUpdate = "UPDATE galeri " + "SET carName = ?, carModel = ?, carColour = ? " + "WHERE carId = ?";
 			preparedStatement = getConnect.prepareStatement(sqlUpdate);
 			preparedStatement.setString(1, galeriEntity.getCarName());
 			preparedStatement.setInt(2, galeriEntity.getCarModel());
 			preparedStatement.setString(3, galeriEntity.getCarColour());
 			preparedStatement.setInt(4, galeriEntity.getCarId());
+			preparedStatement.executeUpdate();
 			
-			int rowsUptated = preparedStatement.executeUpdate();
-			if (rowsUptated > 0)
 				System.out.println("Update Succesfully");
 		} catch (Exception e) {
 			System.err.println("Update ERROR : " + e);
-		}
-	 finally {
+		} finally {
 			getConnect.close();
 		}
 	}
